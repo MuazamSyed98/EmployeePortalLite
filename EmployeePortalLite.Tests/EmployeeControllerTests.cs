f770999f19c7f25d5bd608332cb4c45b987ba496
MuazamSyed98 <syedmua@sheridancollege.ca>
2025-12-10 01:55:42 -0500

    Performed Second Migration and Added Tests

---
+++ EmployeePortalLite.Tests/EmployeeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using EmployeePortalLite.Controllers;
+using EmployeePortalLite.Data;
+using EmployeePortalLite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeePortalLite.Tests
+{
+    [TestClass]
+    public class EmployeeControllerTests
+    {
+        private ApplicationDbContext GetRealContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EmployeePortalLiteDb;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
+        [TestMethod]
+        public async Task Index_ReturnsViewResult()
+        {
+            using var context = GetRealContext();
+
+            context.Employees.Add(new Employee
+            {
+                FullName = "Test User",
+                Email = "test@example.com",
+                Department = "IT",
+                Phone = "1234567890"
+            });
+            await context.SaveChangesAsync();
+
+            var controller = new EmployeesController(context);
+
+            var result = await controller.Index();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
---
+++ EmployeePortalLite.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeePortalLite.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
